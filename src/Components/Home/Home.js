9ea8c3eab630b541724b709314fe1bb90dff9a4e
Oropeza Martinez <O000865@bf.arg.igrupobbva>
2022-08-23 11:46:54 -0300

    estilos de bootstrap al home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,14 +6,14 @@ function Home() {
 
     return(
         <>
-            <div className="container-fluid">
+            <div className="container">
                 <div className="row">
-                    <div class="col-6">
+                    <div className="col-6">
                             <h1>Hola! Bienvenidx</h1>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Adipiscing dignissim ac et eleifend lacus, rhoncus, dignissim sagittis. Tellus ac a, elementum ut. Tellus a morbi tincidunt ultricies malesuada eget turpis. Lacus enim non enim, velit hac turpis interdum arcu. Suspendisse at vel ultrices amet arci enim lectus porttitor ut.</p>
-                            <button className="btn"> Contactanos </button>
+                            <button className="btn btn-danger"> Contactanos </button>
                     </div>
-                    <div class="col-s6">
+                    <div className="col-6">
                         <img src={bienvenida} className="" alt="" />
                     </div>                    
                 </div>
