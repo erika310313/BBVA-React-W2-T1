abdc8b46fbec344a5174993adefbfbc1931fc346
denissemaiz <denissemaiz@gmail.com>
2022-08-24 00:29:07 -0300

    FST1W2-62
    Agregué hoja de estilos. Reemplacé los <Link> que aun no podía usar por <a> en el Header para visualizar los estilos

--- src/Components/PublicWeb/Header.js
+++ src/Components/PublicWeb/Header.js
@@ -8,7 +8,7 @@ function Header(){
     return (
       <header>
           <nav className='gr-header'>
-            <div className='group33'>
+            <div className='gr-logo'>
               <div className='ellipse8'>
                 <img src={ ellipse } className="#" alt="logo" />
               </div>
@@ -22,29 +22,37 @@ function Header(){
             </div>
               <ul className='menu'>
                 <li className='menu-item'>
-                  {/*  <Link className="text-header" to="/inicio"> */}Inicio{/* </Link> */}
+                  <a className="text-header inicio" href='#'>Inicio</a>
+                  {/* <Link className="text-header" to="/inicio">Inicio</Link> */}
                 </li>
-                <li className='menu-item text-header'>
-                 {/*  <Link className="text-header" to="/nosotros"> */}Nosotros{/* </Link> */}
+                <li className='menu-item'>
+                  <a className="text-header" href="#">Nosotros</a>
+                  {/* <Link className="text-header" to="/nosotros">Nosotros</Link> */}
                 </li>
-                <li className='menu-item text-header'>
-                  {/* <Link className="text-header" to="/novedades"> */}Novedades{/* </Link> */}
+                <li className='menu-item'>
+                  <a className="text-header" href='#'>Novedades</a>
+                  {/* <Link className="text-header" to="/novedades">Novedades</Link> */}
                 </li>
-                {/* <li className='menu-item text-header'>
-                  <Link className="text-header" to="/testimonios">Testimonios</Link>
-                </li> */}
-                <li className='menu-item text-header'>
-                  {/* <Link className="text-header" to="/contacto"> */}Contacto{/* </Link> */}
+                <li className='menu-item'>
+                  <a className="text-header" href='#'>Testimonios</a>
+                  {/* <Link className="text-header" to="/testimonios">Testimonios</Link> */}
                 </li>
-                <li className='menu-item text-header'>
-                  {/* <Link className="text-header" to="/campanias"> */}Campañas{/* </Link> */}
+                <li className='menu-item'>
+                  <a className="text-header" href="#">Contacto</a>
+                  {/* <Link className="text-header" to="/contacto">Contacto</Link> */}
                 </li>
-                {/* <li className='menu-item'>
-                  <Link className="text-header" to="/login">Log In</Link>
+                <li className='menu-item'>
+                  <a className="text-header" href='#'>Campañas</a>
+                  {/* <Link className="text-header" to="/campanias">Campañas</Link> */}
                 </li>
-                <li className='menu-item text-header register'>
-                  <Link className="text-header" to="/register">Registrate</Link>
-                </li> */}
+                <li className='menu-item login'>
+                  <a className="text-header" href='#'>Log In</a>
+                  {/* <Link className="text-header" to="/login">Log In</Link> */}
+                </li>
+                <li className='menu-item register'>
+                  <a className="text-header white" href="#">Registrate</a>
+                  {/* <Link className="text-header" to="/register">Registrate</Link> */}
+                </li>
               </ul>
           </nav>
       </header>
