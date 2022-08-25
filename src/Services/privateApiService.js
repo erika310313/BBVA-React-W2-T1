305da4b5eb09157a1fc984f2055bcd52b58cc5f8
DaianaDAG <daigutierrez.3@gmail.com>
2022-08-25 13:22:37 -0300

    FST1W2-26

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const config = {
     headers: {
-        Group: 01                //Aqui va el ID del equipo!!
+        //Group: 01                //Aqui va el ID del equipo!!
     }
 }
 
@@ -11,5 +11,16 @@ const Get = () => {
     .then(res => console.log(res))
     .catch(err => console.log(err))
 }
+
+
+const token = () => {
+    let token = localStorage.getItem('token');
+    if (token) {
+        const headerAuthorization = {
+            bearer: "Bearer" + token
+        }
+    }
+}
+
 
 export default Get
